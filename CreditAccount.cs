9fb92bdf93ebb25b2e7ee53f2b9b50b199dfe6e2
SefikaBoz <sefikaboz12@gmail.com>
2017-05-05 17:38:53 +0300

    Create CreditAccount.cs

---
+++ CreditAccount.cs
@@ -0,0 +1,18 @@
+# 05-05-2017
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo6
+{
+    class CreditAccount
+    {
+        public ulong AccountNo;
+        public double Limit;
+        public string CardOwner;
+    }
+    
+}
